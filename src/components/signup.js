5d69b7e85fd6c6f66a8dc24c478e65e5a246497a
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-04-20 17:44:20 +0530

    signup logic edited

--- src/components/signup.js
+++ src/components/signup.js
@@ -7,7 +7,14 @@ import { useState } from "react";
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const signIn = async () => {};
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log(auth?.email);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="loginpage">
       <div className="log">
